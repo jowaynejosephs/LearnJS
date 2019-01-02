aaa553d8ca3dd013bb1da51a0db45fa9ea14f6ba
Josephs <I814319@ngic.com>
2019-01-02 11:37:57 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+ 
+/*console.log("example");
+console.error("error")
+console.log("%cHello World", "color:green;background:black")*//*Inserting css into the console */
+
+/*this is a comment */
+
+/*var question1="what is your ";
+var question2=prompt("question age/name")  //This is asked first, the answer stored back into the question2 variable
+var question = question1+question2+"?";
+var myName=prompt(question);*/
+
+//Ask for one value
+/*var a=prompt("first number")
+var b=prompt("second number")
+
+//Ask for a second value
+
+//Add them both
+var total=Number(a)+Number(b)
+alert(total)
+//display in an alert
+*/
+
+/*var a =prompt("first")
+var b=prompt("second")
+var message=(a>b)?"A is larger" +a :"False;"*/
+
+//ARRAYS
+
+/*var myArray=["string",100,false]// Called a literal array
+
+var myArray1=new Array("string",1000,true)
+var myArray2=new Array();
+
+
+myArray2[0]="First item"
+console.log(myArray2[0])
+
+var goals=["study javascript", "clean room", "gym","study c#", "read"]
+var progress=["done","in progress", "to do"];
+
+var question1=prompt("which task 1-4");
+var question2=prompt("status 1=done 2=in progress 3=to do")*/
+
+
+
+//Practice Prompts
+
+/*var question=prompt("What is your name?")
+answer=question
+alert("Hello "+ answer +"."+ " Welcome to the program");
+console.log(answer)*/
+
+
+
+
+
+
+
